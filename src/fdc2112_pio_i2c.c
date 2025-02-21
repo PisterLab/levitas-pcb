718e1a5c1cfc66245f22051ad961017a1d3bde09
dteal <dteal@dteal.org>
2025-02-20 23:04:36 -0800

    Have 4 FDC2112 chips working at once via PIO I2C

---
+++ src/fdc2112_pio_i2c.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+//#include "hardware/i2c.h"
+// I2C over PIO
+#include "pio_i2c.h"
+
+// I2C address of FDC2112 chip
+#define FDC2112_I2C_ADDR 0x2b
+// I2C-accessible registers of FDC2112 chip
+#define FDC2112_REG_DATA_CH0 0x00
+#define FDC2112_REG_RCOUNT_CH0 0x08
+#define FDC2112_REG_OFFSET_CH0 0x0c
+#define FDC2112_REG_SETTLECOUNT_CH0 0x10
+#define FDC2112_REG_CLOCK_DIVIDERS_CH0 0x14
+#define FDC2112_REG_STATUS 0x18
+#define FDC2112_REG_ERROR_CONFIG 0x19
+#define FDC2112_REG_CONFIG 0x1a
+#define FDC2112_REG_MUX_CONFIG 0x1b
+#define FDC2112_REG_RESET_DEV 0x1c
+#define FDC2112_REG_DRIVE_CURRENT_CH0 0x1e
+#define FDC2112_REG_MANUFACTURER_ID 0x7e
+#define FDC2112_REG_DEVICE_ID 0x7f
+
+// write a 16-bit value to the given register
+void fdc2112_write_register(PIO pio, uint sm, uint8_t reg, uint16_t data){
+    uint8_t buf[3];
+    buf[0] = reg;
+    buf[1] = data >> 8; // send MSB byte first
+    buf[2] = data & 0xff; // send LSB byte last
+    pio_i2c_write_blocking(pio, sm, FDC2112_I2C_ADDR, buf, 3, false);
+}
+
+// read a 16-bit value from given register
+uint16_t fdc2112_read_register(PIO pio, uint sm, uint8_t reg){
+    uint8_t buf_reg[1];
+    buf_reg[0] = reg;
+    uint8_t buf_data[] = {0x00, 0x00};
+    pio_i2c_write_blocking(pio, sm, FDC2112_I2C_ADDR, buf_reg, 1, true); // shouldn't stop
+    pio_i2c_read_blocking(pio, sm, FDC2112_I2C_ADDR, buf_data, 2);
+    // result is MSB then LSB byte
+    return (((uint16_t)buf_data[0]) << 8) | ((uint16_t)buf_data[1]);
+}
+
+static void fdc2112_startup(PIO pio, uint sm){
+    // Chip is in sleep state by default and must be enabled.
+    // To do so, set a bit in the CONFIG register, and set the
+    // rest of the configuration while we're at it.
+
+    // Set register 0x1a to 00010100 00000001 = 0x1401
+    // (sense on channel 0, no sleep mode, full current, internal oscillator)
+
+
+    // F_ref = 43MHz -> 2866 cycles per sample at 15kHz -> 10 bits precision? -> 1024 clock cycles for rcount -> rcount = 0x0040
+    //fdc2112_write_register(i2c_inst, FDC2112_REG_RCOUNT_CH0, 0x8329);
+    // rcount > 8
+    fdc2112_write_register(pio, sm, FDC2112_REG_RCOUNT_CH0, 0x0040);
+
+    //fdc2112_write_register(i2c_inst, FDC2112_REG_SETTLECOUNT_CH0, 0x000a);
+    fdc2112_write_register(pio, sm, FDC2112_REG_SETTLECOUNT_CH0, 0x000a);
+    // settlecount > 3
+
+    fdc2112_write_register(pio, sm, FDC2112_REG_CLOCK_DIVIDERS_CH0, 0x2002);
+    fdc2112_write_register(pio, sm, FDC2112_REG_ERROR_CONFIG, 0x0000);
+    // 0000 0010 0000 1101 = 0x020d
+    fdc2112_write_register(pio, sm, FDC2112_REG_MUX_CONFIG, 0x020d);
+    fdc2112_write_register(pio, sm, FDC2112_REG_DRIVE_CURRENT_CH0, 0x0000);
+    // 0001 0100 0000 0001 = 0x1401
+    fdc2112_write_register(pio, sm, FDC2112_REG_CONFIG, 0x1401);
+}
+
+int main() {
+
+    // enable all available stdio outputs
+    // (currently only USB in CMakeLists.txt)
+    // USB serial port defaults to 115200 baud
+    stdio_init_all();
+
+    // set up LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    // set up I2C
+    PIO pio = pio0; // choose a PIO
+    uint sm0 = 0; // choose PIO state machine
+    uint sm1 = 1; // choose PIO state machine
+    uint sm2 = 2; // choose PIO state machine
+    uint sm3 = 3; // choose PIO state machine
+    uint offset = pio_add_program(pio, &pio_i2c_program); // load program into PIO memory, return instruction memory offset it is loaded at
+    pio_i2c_program_init(pio, sm0, offset, 4, 5); // start PIO state machine at given instruction offset with given SDA and SCL pin
+    pio_i2c_program_init(pio, sm1, offset, 6, 7); // start PIO state machine at given instruction offset with given SDA and SCL pin
+    pio_i2c_program_init(pio, sm2, offset, 8, 9); // start PIO state machine at given instruction offset with given SDA and SCL pin
+    pio_i2c_program_init(pio, sm3, offset, 10, 11); // start PIO state machine at given instruction offset with given SDA and SCL pin
+
+    //i2c_init(i2c0, 400 * 1000); // i2c fast mode
+    //i2c_init(i2c1, 400 * 1000); // i2c fast mode
+    //gpio_set_function(4, GPIO_FUNC_I2C); // i2c0 SDA, Pico 6, MISO-d
+    //gpio_set_function(5, GPIO_FUNC_I2C); // i2c0 SCL, Pico 7, CS-d
+    //gpio_set_function(6, GPIO_FUNC_I2C); // i2c1 SDA, Pico 9, SCLK-d
+    //gpio_set_function(7, GPIO_FUNC_I2C); // i2c1 SCL, Pico 10, MOSI-d
+    //gpio_set_function(8, GPIO_FUNC_I2C); // i2c0 SDA, Pico 11, SDA-d
+    //gpio_set_function(9, GPIO_FUNC_I2C); // i2c0 SCL, Pico 12, SCL-d
+    //gpio_set_function(10, GPIO_FUNC_I2C); // i2c1 SDA, Pico 14, SDA4-d
+    //gpio_set_function(11, GPIO_FUNC_I2C); // i2c1 SCL, Pico 15, SCL4-d
+    //gpio_pull_up(4);
+    //gpio_pull_up(5);
+    //gpio_pull_up(6);
+    //gpio_pull_up(7);
+    //gpio_pull_up(8);
+    //gpio_pull_up(9);
+    //gpio_pull_up(10);
+    //gpio_pull_up(11);
+
+    // call many times just in case
+    sleep_ms(500);
+    fdc2112_startup(pio, sm0);
+    fdc2112_startup(pio, sm1);
+    fdc2112_startup(pio, sm2);
+    fdc2112_startup(pio, sm3);
+
+    gpio_put(PICO_DEFAULT_LED_PIN, true);
+    while(true){
+        //gpio_put(PICO_DEFAULT_LED_PIN, true);
+        //gpio_put(PICO_DEFAULT_LED_PIN, false);
+        sleep_ms(10);
+
+        // TODO: put printing/USB on second core
+
+        uint16_t c0 = fdc2112_read_register(pio, sm0, FDC2112_REG_DATA_CH0);
+        uint16_t c1 = fdc2112_read_register(pio, sm1, FDC2112_REG_DATA_CH0);
+        uint16_t c2 = fdc2112_read_register(pio, sm2, FDC2112_REG_DATA_CH0);
+        uint16_t c3 = fdc2112_read_register(pio, sm3, FDC2112_REG_DATA_CH0);
+        printf("Data: 0x%02x%02x 0x%02x%02x 0x%02x%02x 0x%02x%02x\n", (c0>>8), (c0&0xff), (c1>>8), (c1&0xff), (c2>>8), (c2&0xff), (c3>>8), (c3&0xff));
+    }
+
+    return 0;
+}
+
---
+++ src/pio_i2c.c
@@ -0,0 +1,211 @@
+/**
+ * Part of a library to run I2C master from a PIO state machine.
+ * (use with pio_i2c.h and pio_i2c.pio)
+ *
+ * Originally from (then heavily commented and slightly tweaked)
+ * https://github.com/raspberrypi/pico-examples/tree/master/pio/i2c
+ * Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pio_i2c.h"
+
+// bit indices of assorted parts of instruction
+// (see PIO program)
+const int PIO_I2C_ICOUNT_LSB = 10;
+const int PIO_I2C_FINAL_LSB  = 9;
+const int PIO_I2C_DATA_LSB   = 1;
+const int PIO_I2C_NAK_LSB    = 0;
+
+// determine whether state machine interrupt was set
+// (will happen if unexpected NAK was observed)
+bool pio_i2c_check_error(PIO pio, uint sm) {
+    return pio_interrupt_get(pio, sm);
+}
+
+// manually restart state machine after NAK-triggered interrupt
+void pio_i2c_resume_after_error(PIO pio, uint sm) {
+    // empty FIFO buffer
+    pio_sm_drain_tx_fifo(pio, sm);
+    // restart by manually making state machine execute an unconditional jump
+    // to the desired entry point (beginning of wrap loop)
+    pio_sm_exec(pio, sm, (pio->sm[sm].execctrl & PIO_SM0_EXECCTRL_WRAP_BOTTOM_BITS) >> PIO_SM0_EXECCTRL_WRAP_BOTTOM_LSB);
+    // clear interrupt
+    pio_interrupt_clear(pio, sm);
+}
+
+// enable or disable state machine autopush
+// (when enabled, read data from I2C)
+// NOTE: POTENTIAL RACE CONDITION
+void pio_i2c_rx_enable(PIO pio, uint sm, bool en) {
+    if (en)
+        hw_set_bits(&pio->sm[sm].shiftctrl, PIO_SM0_SHIFTCTRL_AUTOPUSH_BITS);
+    else
+        hw_clear_bits(&pio->sm[sm].shiftctrl, PIO_SM0_SHIFTCTRL_AUTOPUSH_BITS);
+}
+
+// write a 16-bit value to FIFO (blocking), don't check for error
+static inline void pio_i2c_put16(PIO pio, uint sm, uint16_t data) {
+    // wait if FIFO is full
+    while (pio_sm_is_tx_fifo_full(pio, sm))
+        ;
+    // some versions of GCC dislike this
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#endif
+    *(io_rw_16 *)&pio->txf[sm] = data;
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+}
+
+// write a 16-bit value to FIFO (blocking),
+// but error out on NAK-triggered interrupt
+void pio_i2c_put_or_err(PIO pio, uint sm, uint16_t data) {
+    // block while state machine FIFO is full
+    while (pio_sm_is_tx_fifo_full(pio, sm))
+        if (pio_i2c_check_error(pio, sm))
+            return;
+    if (pio_i2c_check_error(pio, sm))
+        return;
+    // some versions of GCC dislike this
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#endif
+    *(io_rw_16 *)&pio->txf[sm] = data;
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+}
+
+// get data from FIFO
+uint8_t pio_i2c_get(PIO pio, uint sm) {
+    return (uint8_t)pio_sm_get(pio, sm);
+}
+
+// make state machine emit I2C start signal
+void pio_i2c_start(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 1u << PIO_I2C_ICOUNT_LSB);                      // tell state machine to execute the next 1+1=2 commands
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD0]); // while clock is high (idling), pull SDA low to signal start
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD0]); // also pull clock low to be ready to send data
+}
+
+// make state machine emit I2C stop signal
+void pio_i2c_stop(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 2u << PIO_I2C_ICOUNT_LSB);                      // tell state machine to execute the next 1+2=3 commands
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD0]); // SDA is unknown; pull it low while clock is down (which it should already be)
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD0]); // clock high while SDA low
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD1]); // SDA high while clock is low to return to idle
+};
+
+// replace a stop with this in order to later do a restart
+void pio_i2c_restart_stop(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 1u << PIO_I2C_ICOUNT_LSB);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD1]);
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD1]);
+};
+
+// make state machine emit I2C repeated start signal
+// NOTE: NOT USED
+void pio_i2c_repstart(PIO pio, uint sm) {
+    pio_i2c_put_or_err(pio, sm, 3u << PIO_I2C_ICOUNT_LSB);                      // tell state machine to execute the next 1+3=4 commands
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD1]); // while SCL is low, ensure SDA is high
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD1]); // bring SCL high
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC1_SD0]); // bring SDA low while SCL is high (the stop signal)
+    pio_i2c_put_or_err(pio, sm, set_scl_sda_program_instructions[I2C_SC0_SD0]); // bring SCL back down
+}
+
+// wait until state machine has nothing left in FIFO or emits IRQ error
+static void pio_i2c_wait_idle(PIO pio, uint sm) {
+    // clear the TXSTALL bit (which is set when state machine stalls on empty TX FIFO)
+    pio->fdebug = 1u << (PIO_FDEBUG_TXSTALL_LSB + sm);
+    // wait until TXSTALL bit is set again by PIO state machine, or IRQ error
+    while (!(pio->fdebug & 1u << (PIO_FDEBUG_TXSTALL_LSB + sm) || pio_i2c_check_error(pio, sm)))
+        tight_loop_contents(); // SDK no-op function
+}
+
+int pio_i2c_write_blocking(PIO pio, uint sm, uint8_t addr, uint8_t *txbuf, uint len, bool nostop) {
+    int err = 0;
+    // emit start signal
+    pio_i2c_start(pio, sm);
+    // temporarily disable reading data from I2C
+    pio_i2c_rx_enable(pio, sm, false); // TODO remove this
+    // write data, expect ack, 8 bits (address + 0 for writing), don't send master ack
+    pio_i2c_put16(pio, sm, (addr << 2) | 1u);
+    while (len && !pio_i2c_check_error(pio, sm)) {
+        // wait until there's room in the FIFO buffer
+        if (!pio_sm_is_tx_fifo_full(pio, sm)) {
+            --len;
+            // then send data
+            // expect ack (except on last byte?), 8 bits, don't send master ack
+            pio_i2c_put_or_err(pio, sm, (*txbuf++ << PIO_I2C_DATA_LSB) | ((len == 0) << PIO_I2C_FINAL_LSB) | 1u);
+        }
+    }
+    if(nostop){
+        pio_i2c_restart_stop(pio, sm);
+    }else{
+        // emit stop signal
+        pio_i2c_stop(pio, sm);
+    }
+    // wait until state machine is done sending everything
+    pio_i2c_wait_idle(pio, sm);
+    // if hit an error, restart
+    if (pio_i2c_check_error(pio, sm)) {
+        err = -1;
+        pio_i2c_resume_after_error(pio, sm);
+        pio_i2c_stop(pio, sm);
+    }
+    return err;
+}
+
+int pio_i2c_read_blocking(PIO pio, uint sm, uint8_t addr, uint8_t *rxbuf, uint len) {
+    int err = 0;
+    // emit start signal
+    pio_i2c_start(pio, sm);
+    // enable reading data from I2C (data is being put into FIFO from state machine)
+    pio_i2c_rx_enable(pio, sm, true); // TODO remove this
+    // empty data input FIFO
+    while (!pio_sm_is_rx_fifo_empty(pio, sm))
+        (void)pio_i2c_get(pio, sm);
+    // write data, expect ack, 8 bits (address + 1 for reading), don't send master ack
+    pio_i2c_put16(pio, sm, (addr << 2) | 3u);
+    // send data
+    // use tx_remain variable to track how many bytes to tell state machine to expect
+    // use len to track how many bytes we've read out
+    uint32_t tx_remain = len;
+    bool first = true;
+    while ((tx_remain || len) && !pio_i2c_check_error(pio, sm)) { // in very fast loop...
+        // when the FIFO buffer has room, send command to tell state machine to read another byte
+        if (tx_remain && !pio_sm_is_tx_fifo_full(pio, sm)) {
+            --tx_remain;
+            // send 0xff (doesn't write to bus b/c pullup), expect NAK on final byte, otherwise have master send ack
+            pio_i2c_put16(pio, sm, (0xffu << 1) | (tx_remain ? 0 : (1u << PIO_I2C_FINAL_LSB) | (1u << PIO_I2C_NAK_LSB)));
+        }
+        // when there's stuff available, read it
+        if (!pio_sm_is_rx_fifo_empty(pio, sm)) {
+            if (first) {
+                // Ignore returned address byte
+                (void)pio_i2c_get(pio, sm);
+                first = false;
+            }else {
+                --len;
+                *rxbuf++ = pio_i2c_get(pio, sm);
+            }
+        }
+    }
+    // emit stop signal
+    pio_i2c_stop(pio, sm);
+    // wait until state machine is done sending everything
+    pio_i2c_wait_idle(pio, sm);
+    // if hit an error, restart
+    if (pio_i2c_check_error(pio, sm)) {
+        err = -1;
+        pio_i2c_resume_after_error(pio, sm);
+        pio_i2c_stop(pio, sm);
+    }
+    return err;
+}
+
