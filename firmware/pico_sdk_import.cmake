1c03829898034e6cdf148b0939776d80a53d943f
dteal <dteal@dteal.org>
2025-03-20 13:34:24 -0700

    Add configuration to allow compiling with VS Code

---
+++ firmware/pico_sdk_import.cmake
@@ -0,0 +1,5 @@
+# THIS IS AN EMPTY PLACEHOLDER FILE. DO NOT REMOVE.
+# The Raspberry Pi Pico VS Code extension requires this file to exist
+# in order to recognize this as a Pico project and enable the extension.
+# (see <https://github.com/raspberrypi/pico-vscode/blob/0.17.5/src/extension.mts#L182C28-L182C29>)
+# (This is unused when not using the VS Code extension.)
